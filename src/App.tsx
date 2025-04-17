51bdd754a241a30253d363f1510f6c9e014a21b2
Ezeh Mark <ezehmark@gmail.com>
2025-04-17 19:57:35 +0100

    new build

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  const[clickedItem,setClickedItem]=useState(null);
+  const[clickedItem,setClickedItem]=useState("");
 
   return (
     <><div className="outer">
