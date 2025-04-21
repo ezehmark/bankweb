ad3c3391b5b382cdc000b8095887f5e082f2cc63
Ezeh Mark <ezehmark@gmail.com>
2025-04-21 15:52:03 +0100

    new build

--- src/App.tsx
+++ src/App.tsx
@@ -4,73 +4,85 @@ import "./App.css";
 function App() {
   const topRef = useRef<HTMLDivElement>(null);
   const button2Ref = useRef<HTMLDivElement>(null);
-  const [total,setTotal] = useState("1300450");
+  const [total, setTotal] = useState("1300450");
   const [btc, setBtc] = useState("900000");
   const [eth, setEth] = useState("300000");
   const [sol, setSol] = useState("250000");
+
+  const[prices,setPrices]=useState({BTCUSDT:null,ETHUSDT:null,SOLUSDT:null});
 
   const [btcPrice, setBtcPrice] = useState<string | null>(null);
   const [ethPrice, setEthPrice] = useState<string | null>(null);
   const [solPrice, setSolPrice] = useState<string | null>(null);
 
+  const btcPriceRef = useRef<string | null>(null);
+  const ethPriceRef = useRef<string | null>(null);
+  const solPriceRef = useRef<string | null>(null);
 
+  useEffect(() => {
+    const myWs = new WebSocket("wss://stream.bybitglobal.com/v5/public/spot");
 
+    myWs.onopen = () => {
+      myWs.send(
+        JSON.stringify({
+          op: "subscribe",
+          args: [
+            "publicTrade.BTCUSDT",
+            "publicTrade.ETHUSDT",
+            "publicTrade.SOLUSDT",
+          ],
+        }),
+      );
+    };
 
+    myWs.onmessage = (info) => {
+      const myData = JSON.parse(info.data);
 
-const btcPriceRef = useRef<string | null>(null);
-const ethPriceRef = useRef<string | null>(null);
-const solPriceRef = useRef<string | null>(null);
+      if (myData.topic?.startsWith("publicTrade") && myData.data.length > 0) {
+        const symbol = myData.topic.split(".")[1];
+        const price = parseFloat(myData.data[0].p.toString());
 
-const API_KEY ="d9bbb9c22fa2553ad23fd9e95430ce31f26565427716b263fcc82b3565e90d8a";
+        setPrices((prev) => ({ ...prev, [symbol]: price }));
 
-useEffect(()=>{
-const ws = new WebSocket(`wss://wss.coincap.io/prices?assets=bitcoin,ethereum,solana&apiKey=${API_KEY}`);	
-ws.onmessage =(info)=>{
-const data = JSON.parse(info.data);
+        const liveBtc = symbol == "BTCUSDT" ? price : prices?.BTCUSDT || btcPriceRef.current;
+        const liveEth = symbol=="ETHUSDT" ? price : prices?.ETHUSDT || ethPriceRef.current;
+        const liveSol = symbol=="SOLUSDT" ? price : prices?.SOLUSDT || solPriceRef.current;
 
+        if (liveBtc && !isNaN(parseFloat(liveBtc))) {
+          btcPriceRef.current = liveBtc;
+          setBtcPrice(liveBtc);
+	  console.log("BTCUSDT price updated using Bybit ws, as",liveBtc)
+        }
 
+        if (liveEth && !isNaN(parseFloat(liveEth))) {
+          ethPriceRef.current = liveEth;
+          setEthPrice(liveEth);
+        }
 
-const btcLive = data.bitcoin || btcPriceRef.current;
-const ethLive = data.ethereum || ethPriceRef.current;
-const solLive = data.solana || solPriceRef.current;
+        if (liveSol && !isNaN(parseFloat(liveSol))) {
+          solPriceRef.current = liveSol;
+          setSolPrice(liveSol);
+        }
 
-if(btcLive && !isNaN(parseFloat(btcLive))){
-setBtcPrice(btcLive);
-btcPriceRef.current = btcLive;
-console.log("✅ WS has updated bitcoin price, as", btcLive)}
-else{
-console.log("❌ WS returned empty data for bitcoin as", data.bitcoin)}
+        const myBtcValue = Number(liveBtc) * 12;
+        const myEthValue = Number(liveEth) * 120;
+        const mySolValue = Number(liveSol) * 500;
+        const totalValue = myBtcValue + myEthValue + mySolValue;
 
-if(ethLive && !isNaN(parseFloat(ethLive))){
-setEthPrice(ethLive);
-ethPriceRef.current = ethLive}
+        setTotal(totalValue.toString());
 
-if(solLive && !isNaN(parseFloat(solLive))){
-setSolPrice(solLive);
-solPriceRef.current=solLive}
-
-
-
-const myBtcValue = Number(btcLive)*12;
-const myEthValue = Number(ethLive)*120;                   
-const mySolValue = Number(solLive)*500;
-const totalValue = myBtcValue + myEthValue + mySolValue;
-
-setTotal(totalValue.toString());
-
-setBtc(myBtcValue.toString());
-setEth(myEthValue.toString());
-setSol(mySolValue.toString());}
-
-
-return ()=>ws.close()
-},[]);
-
+        setBtc(myBtcValue.toString());
+        setEth(myEthValue.toString());
+        setSol(mySolValue.toString());
+      }
+    };
+    return () => myWs.close();
+  }, []);
 
   const handleAnim1 = () => {
     if (topRef.current && button2Ref.current) {
-	    const top = topRef.current;
-	    const button2 = button2Ref.current;
+      const top = topRef.current;
+      const button2 = button2Ref.current;
       top.classList.remove("animClass");
       void top.offsetWidth;
       top.classList.add("animClass");
@@ -83,8 +95,8 @@ return ()=>ws.close()
 
   const handleAnim2 = () => {
     if (topRef.current && button2Ref.current) {
-	    const top = topRef.current;
-            const button2 = button2Ref.current;
+      const top = topRef.current;
+      const button2 = button2Ref.current;
       button2.style.backgroundColor = "#ef9800";
       top.classList.remove("animClass2");
 
@@ -104,13 +116,23 @@ return ()=>ws.close()
         <div className="title">Bank Web</div>
         <div className="outer">
           <div className="perspective">
-	  <div className="button2" ref={button2Ref}onClick={()=>{handleAnim2()}}>See bank</div>
+            <div
+              className="button2"
+              ref={button2Ref}
+              onClick={() => {
+                handleAnim2();
+              }}
+            >
+              See bank
+            </div>
             <div className="top" ref={topRef}>
               <div className="button1" onClick={() => handleAnim1()}>
                 See coins
               </div>
               <div className="crypto-face">
-                <div className="total">{Number(total).toLocaleString("en-us")} USD</div>
+                <div className="total">
+                  {Number(total).toLocaleString("en-us")} USD
+                </div>
                 <div className="wallet">Crypto wallet</div>
                 <div className="coins-list">
                   <div
@@ -124,7 +146,8 @@ return ()=>ws.close()
                       <img src="https://i.postimg.cc/7L1GY17c/download-3.png" />
                     </div>
                     <div className="coin-name">
-                      <b style={{ fontWeight:"normal", fontSize: 10 }}>12 </b>BTC
+                      <b style={{ fontWeight: "normal", fontSize: 10 }}>12 </b>
+                      BTC
                     </div>
                     <div className="price">
                       ~ {Number(btc).toLocaleString("en-us")}
@@ -137,8 +160,8 @@ return ()=>ws.close()
                       <img src="https://i.postimg.cc/PqqWcF0y/Ethereum-logo.png" />
                     </div>
                     <div className="coin-name">
-                      
-                      <b style={{ fontWeight:"normal", fontSize: 10 }}>120 </b>ETH
+                      <b style={{ fontWeight: "normal", fontSize: 10 }}>120 </b>
+                      ETH
                     </div>
                     <div className="price">
                       ~ {Number(eth).toLocaleString("en-us")}
@@ -160,7 +183,8 @@ return ()=>ws.close()
                       />
                     </div>
                     <div className="coin-name">
-                      <b style={{ fontWeight:"normal", fontSize: 10 }}>500 </b>SOL
+                      <b style={{ fontWeight: "normal", fontSize: 10 }}>500 </b>
+                      SOL
                     </div>
                     <div className="price">
                       ~ {Number(sol).toLocaleString("en-us")}
@@ -206,15 +230,27 @@ return ()=>ws.close()
             </div>
             <div className="ticker1">
               <div>BTC</div>
-              <div>{btcPrice == null?"Loading..." : Number(btcPrice).toLocaleString("en-us")}</div>
+              <div>
+                {btcPrice == null
+                  ? "Loading..."
+                  : Number(btcPrice).toLocaleString("en-us")}
+              </div>
             </div>
             <div className="ticker1">
               <div>ETH</div>
-              <div>{ethPrice==null?"Loading...":Number(ethPrice).toLocaleString("en-us")}</div>
+              <div>
+                {ethPrice == null
+                  ? "Loading..."
+                  : Number(ethPrice).toLocaleString("en-us")}
+              </div>
             </div>
             <div className="ticker1">
               <div>SOL</div>
-              <div>{solPrice==null?"Loading...":Number(solPrice).toLocaleString("en-us")}</div>
+              <div>
+                {solPrice == null
+                  ? "Loading..."
+                  : Number(solPrice).toLocaleString("en-us")}
+              </div>
             </div>
             <div
               style={{
@@ -225,7 +261,7 @@ return ()=>ws.close()
                 display: "flex",
                 width: "100%",
                 height: 40,
-		color:"#291900",
+                color: "#291900",
                 backgroundColor: "#c77700",
               }}
             >
